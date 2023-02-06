e86fd8952e05530ea833f387b52c7feb7b720bae
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-02-06 13:56:06 +0530

    Add files via upload

---
+++ ArithmeticOperator.js
@@ -0,0 +1,25 @@
+var a = 20;
+var b = 10;
+var add = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+var rem = a%b;
+var pow = a**2;
+
+alert("Value of a is "+a+" Value of b is "+b);
+alert("Value after adding "+a+" and "+b+" is "+add);
+alert("Value after subtracting "+a+" and "+b+" is "+sub);
+alert("Value after multiplying "+a+" and "+b+" is "+mul);
+alert("Value after dividing "+a+" and "+b+" is "+div);
+alert("Value after finding remainder of "+a+" and "+b+" is "+rem);
+alert("Value after finding power of "+a+" by 2 is "+pow);
+
+document.write("Value of a is "+a+" Value of b is "+b+"</br>");
+document.write("Value after adding "+a+" and "+b+" is "+add+"</br>");
+document.write("Value after subtracting "+a+" and "+b+" is "+sub+"</br>");
+document.write("Value after multiplying "+a+" and "+b+" is "+mul+"</br>");
+document.write("Value after dividing "+a+" and "+b+" is "+div+"</br>");
+document.write("Value after finding remainder of "+a+" and "+b+" is "+rem+"</br>");
+document.write("Value after finding power of "+a+" by 2 is "+pow+"</br>");
+
---
+++ RelationalOperator.js
@@ -0,0 +1,26 @@
+var a = 20;
+var b = 10;
+var equal = a==b;
+var less = a<b;
+var greater = a>b;
+var lessEqual = a<=b;
+var greaterEqual = a>=b;
+var notEqual = a!=b;
+var equalType = a===b;
+var notEqualType = a!==b;
+
+alert("Value of a is "+a+" Value of b is "+b);
+alert(a+" == "+b+" are "+equal);
+alert(a+" < "+b+" are "+less);
+alert(a+" > "+b+" are "+greater);
+alert(a+" <= "+b+" are "+lessEqual);
+alert(a+" >= "+b+" are "+greaterEqual);
+alert(a+" != "+b+" are "+notEqual);
+
+document.write(a+" == "+b+" are "+equal+"</br>");
+document.write(a+" < "+b+" are "+less+"</br>");
+document.write(a+" > "+b+" are "+greater+"</br>");
+document.write(a+" <= "+b+" are "+lessEqual+"</br>");
+document.write(a+" >= "+b+" are "+greaterEqual+"</br>");
+document.write(a+" != "+b+" are "+notEqual+"</br>");
+
